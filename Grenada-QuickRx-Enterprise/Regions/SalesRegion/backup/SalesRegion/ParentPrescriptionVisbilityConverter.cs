5312f662e3ed981d0aa33b829816fc7fa5343685
alphaquest2005 <josephbartholomew@outlook.com>
2018-02-15 11:46:58 -0400

    Before suppvm select optimization

---
+++ Grenada-QuickRx-Enterprise/Regions/SalesRegion/backup/SalesRegion/ParentPrescriptionVisbilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SalesRegion
+{
+    public class ParentPrescriptionVisbilityConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is int i ? (i > 0 ? Visibility.Visible : Visibility.Hidden) : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
